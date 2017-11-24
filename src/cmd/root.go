b454962c4a2596909c26b357ddb8e9de023087b4
git-user-name <your-email@hoge.com>
2017-11-25 03:11:28 +0900

    init

---
+++ src/cmd/root.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "go-keisan",
+	Short: "This tool is pretty cool.",
+	Long:  "This tool is a great convenience.",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func init() {
+	cobra.OnInitialize()
+	RootCmd.AddCommand(versionCmd)
+
+	show(RootCmd.Commands())
+}
+
+func show(cmds []*cobra.Command) {
+	fmt.Println("----------")
+	for i, c := range cmds {
+		fmt.Printf("[%d] %s --- %s\n", i, c.Use, c.Short)
+	}
+	fmt.Println("----------")
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of go-keisan",
+	Long:  `All software has versions. This is go-keisan's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("go-keisan v1.0")
+	},
+}
